9b8224014012725ce386d06720d03187d7ca4a8d
zz962123 <zz962126@gmail.com>
2022-05-16 18:02:34 +0900

    custom emotion commit v2

--- src/components/atoms/Button/MuiButton.js
+++ src/components/atoms/Button/MuiButton.js
@@ -1,15 +1,29 @@
+/* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
+/** @jsxImportSource @emotion/react */
+
 import React from 'react';
 import Button from '@mui/material/Button';
 import { css } from '@emotion/react';
+import styled from '@emotion/styled'
 
 export default function MuiButton() {
+    
+    const Basic = ({className}) => { return <Button className={className}>test</Button> }
+    const TestButton = styled(Basic)`
+        color: red;
+    `
+
+    
     return ( 
-        <Button 
+        <div>
+        {/* <Button 
             css = {css`
-                color: #2e8b57;
+                color: red;
             `}
         >
             test
-        </Button>
+        </Button> */}
+        <TestButton></TestButton>
+        </div>
     );
 }
